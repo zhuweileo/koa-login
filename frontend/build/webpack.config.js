ad4651b95313b122d7bacef4b94d09fe3be777d7
zhuwei <zhuwei_leo@163.com>
2019-05-21 22:07:20 +0800

    tijiao

---
+++ frontend/build/webpack.config.js
@@ -0,0 +1,62 @@
+var path = require('path')
+var htmlPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: {
+    index: path.resolve(__dirname,'../src/entry/home.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: '[name].js',
+  },
+  mode: 'production',
+  externals:{
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+      root: 'React' // indicates global variable
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use:{
+          loader: "babel-loader",
+        }
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {loader: 'style-loader'},
+          {loader: 'css-loader'},
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          // fallback to style-loader in development
+          process.env.NODE_ENV === 'production'? MiniCssExtractPlugin.loader: 'style-loader',
+          "css-loader",
+          "sass-loader",
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
+    new htmlPlugin({
+      filename: "index.html",
+      template: path.resolve(__dirname,"../src/templates/home.html"),
+    }),
+  ]
+}
