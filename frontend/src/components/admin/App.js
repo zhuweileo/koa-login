175c0b087736392fc9c98c570e779d5607d3d433
zhuwei <zhuwei_leo@163.com>
2019-05-25 16:17:35 +0800

    完成admin前端部分

--- frontend/src/components/admin/App.js
+++ frontend/src/components/admin/App.js
@@ -1,9 +1,11 @@
-import { Layout,  Breadcrumb ,Tabs } from 'antd';
+import { Layout, Breadcrumb, Tabs } from 'antd';
 import Mheader from '../common/Mheader'
 import Mfooter from '../common/Mfooter'
 import NormalLoginForm from './NormalLoginForm'
-const {Content} = Layout;
+import RegistrationForm from './RegistrationForm'
+import "./index.scss"
 
+const { Content } = Layout;
 const TabPane = Tabs.TabPane;
 
 export default function App(params) {
@@ -15,13 +17,16 @@ export default function App(params) {
           <Breadcrumb.Item>Admin</Breadcrumb.Item>
         </Breadcrumb>
         <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
-          <Tabs defaultActiveKey="1" >
-            <TabPane tab="登陆" key="1">
-              <NormalLoginForm/>
-            </TabPane>
-            <TabPane tab="注册" key="2">
-            </TabPane>
-          </Tabs>,
+          <div className="tab-con">
+            <Tabs defaultActiveKey="1" >
+              <TabPane tab="登陆" key="1">
+                <NormalLoginForm />
+              </TabPane>
+              <TabPane tab="注册" key="2">
+                <RegistrationForm/>
+              </TabPane>
+            </Tabs>,
+          </div>
         </div>
       </Content>
       <Mfooter />
